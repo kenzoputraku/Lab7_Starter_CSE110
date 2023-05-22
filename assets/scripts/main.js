9cac93662bd9c86bd643d788d0693d3cbb11c75b
kenzoputraku <kenzo270403@gmail.com>
2023-05-21 23:09:41 -0700

    finished explore

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -45,15 +45,30 @@ function initializeServiceWorker() {
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
-  // B2. TODO - Listen for the 'load' event on the window object.
-  // Steps B3-B6 will be *inside* the event listener's function created in B2
-  // B3. TODO - Register './sw.js' as a service worker (The MDN article
-  //            "Using Service Workers" will help you here)
-  // B4. TODO - Once the service worker has been successfully registered, console
-  //            log that it was successful.
-  // B5. TODO - In the event that the service worker registration fails, console
-  //            log that it has failed.
-  // STEPS B6 ONWARDS WILL BE IN /sw.js
+  if("serviceWorker" in navigator) {
+    // B2. TODO - Listen for the 'load' event on the window object.
+    window.addEventListener('load', async function (){
+    // Steps B3-B6 will be *inside* the event listener's function created in B2
+    try{
+    // B3. TODO - Register './sw.js' as a service worker (The MDN article
+    //            "Using Service Workers" will help you here)
+      const registration = await navigator.serviceWorker.register("./sw.js");
+
+      // B4. TODO - Once the service worker has been successfully registered, console
+      //            log that it was successful.
+      console.log("Service worker has been successfully registered");
+    
+    } catch (error) {
+        // B5. TODO - In the event that the service worker registration fails, console
+      //            log that it has failed.
+      // STEPS B6 ONWARDS WILL BE IN /sw.js
+      console.error(`Registration failed with ${error}`);
+  }
+
+  });
+  
+  }
+
 }
 
 /**
@@ -69,7 +84,7 @@ async function getRecipes() {
   // A1. TODO - Check local storage to see if there are any recipes.
   //            If there are recipes, return them.
   /**************************/
-  let recipes = localStorage.getItem("recipes")
+  let recipes = localStorage.getItem("recipes");
   if (recipes != null) {
     return JSON.parse(recipes);
   }
@@ -111,11 +126,11 @@ async function getRecipes() {
   // A10. TODO - Log any errors from catch using console.error
   // A11. TODO - Pass any errors to the Promise's reject() function
         let response = await fetch(url);
-        response_json = await response.json();
-        recipes.push(response);
-        if(i == RECIPE_URLS.length){
-          saveRecipesToStorage(recipes);
-          resolve(recipes);
+        response = await response.json();
+        arr.push(response);
+        if(arr.length == RECIPE_URLS.length){
+          saveRecipesToStorage(arr);
+          resolve(arr);
         }
 
       } catch (error){
